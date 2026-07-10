a83fab0cdafdc099b6d1dc33e4298156f124540a
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-10 16:55:13 +1000

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isalpha - checks if variable is a character
+*@c: the variable to check
+*
+*returns: 1 if c is a letter, 0 otherwise
+*/
+int _isalpha(int c)
+{
+	if(c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z)
+	{
+		return (1);
+	}
+	return (0);
+}

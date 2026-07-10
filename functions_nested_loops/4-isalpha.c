1ab0a7dbaf33146c78a214e5295f90cf3813a36b
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-10 17:04:33 +1000

    task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -9,11 +9,11 @@ int _isalpha(int c)
 {
 	if(c >= 'a' && c <= 'z')
 	{
-		if(c >= 'A' && c <= 'Z')
-		{
-			return(1);
-		}
-		return (1);
+		return(1);
+	}
+	if(c >= 'A' && c<= 'Z')
+	{
+		return(1);
 	}
 	return (0);
 }

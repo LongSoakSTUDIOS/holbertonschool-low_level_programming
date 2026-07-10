2e5c150dd7e5e80f380060c192288aae1d207d22
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-10 16:39:43 +1000

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+*print_alphabet - checks for lowercase character
+*
+*Return: always 0.
+*/
+int main(void)
+{
+	int c;
+	
+	c = _islower('H');
+	_putchar(r + '0');
+	c = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);	
+}
+

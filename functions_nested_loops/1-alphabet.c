1a34fedb3b403d71188a66cb7680814e2cc5a285
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-10 15:08:40 +1000

    task 0

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - prints alphabet in lowercase followed by a new line
+*
+*Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char letter;
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return;
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,4 +3,6 @@
 
 int _putchar(char c);
 
+void print_alphanet(void);
+
 #endif

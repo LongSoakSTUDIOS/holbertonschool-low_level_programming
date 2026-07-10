a1da33bb1e7ad02fc568264ef691a22b1757f762
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-10 16:14:56 +1000

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet in lowercase followed by a new line
+*
+*Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+	char letter;
+	int counter;
+
+	letter = 'a';
+	counter = '0';
+	while (counter >= '9')
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	_putchar('\n');
+	counter++;
+	}
+}

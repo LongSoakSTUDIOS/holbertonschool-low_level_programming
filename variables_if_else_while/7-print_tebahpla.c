3fdb2fcdd9b118f8705d76d3fc218e8a9214cad0
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-10 10:58:10 +1000

    renaming task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet in reverse
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char letter;
+
+	letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}

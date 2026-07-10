2d5a81959729eb71cf01bc68734caa6e44907be7
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-10 11:44:07 +1000

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - printsa all possible combinations of single-digit numbers
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int number;
+
+	number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		putchar(',');
+		putchar(' ');
+		number++;
+	}
+	return (0);
+
+}

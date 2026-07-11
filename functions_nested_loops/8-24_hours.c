fb0334c09c7c97801440de571ac17bfb0bb85ff1
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-12 09:59:46 +1000

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+*jack_bauer - prints every minute and hour until 23:59
+*
+*Return: always 0
+*/
+int jack_bauer(void)
+{
+	int hour;
+	int minute;
+	
+	hour = 0;
+	
+	while (hour < 24)
+	{
+		minute = 0;
+
+		while (minute < 60)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 +'0');
+			_putchar('\n');
+			minute++;
+		}
+		hour++;
+	}
+
+	putchar(hour);
+	putchar(':');
+	putchar(minute);
+	putchar('\n');
+
+	return (0);	
+}

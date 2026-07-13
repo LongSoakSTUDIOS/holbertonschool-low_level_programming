0df98cc42322d713182230f435a9b6d3d9762e92
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-13 14:49:11 +1000

    betty compliant

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 /**
-*main - runs a simple calulator capable of adding, subtracting, multiplying and dividing
+*main - runs a simple calulator
 *
 *Returns: always 0
 */
 int main(void)
 {
-    int response;
-    int responded; /*0 for false, 1 for true*/
+	int response;
+	int responded; /*0 for false, 1 for true*/
 
-    response = 0;
-    responded = 0;
-    printf("Simple Calculator \n 1) Add \n 2) Subtract \n 3) Multiply \n 4) Divide \n 0) Quit \n");
-    while (responded == 0)
-    {
-        printf("Choice: ");
-        scanf("%d", &response);
-        if(response > 5 || response < 0)
-        {
-            printf("Please enter a valid choice: ");
-            responded = 0;
+	response = 0;
+	responded = 0;
+	printf("Simple Calculator \n 1) Add \n 2) Subtract \n 3) Multiply \n 4) Divide \n 0) Quit \n");
+	while (responded == 0)
+	{
+		printf("Choice: ");
+		scanf("%d", &response);
+		if (response > 5 || response < 0)
+		{
+			printf("Please enter a valid choice: ");
+			responded = 0;
 
-        }    
-        if (response == 0)
-        {
-            printf("Bye!\n");
-            responded = 1;
-            return(0);
-        }    
-    }
-    return (0);
+		}
+		if (response == 0)
+		{
+			printf("Bye!\n");
+			responded = 1;
+			return (0);
+		}
+	}
+	return (0);
 }

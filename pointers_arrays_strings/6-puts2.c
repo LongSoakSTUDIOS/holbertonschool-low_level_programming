d48aee3ea2f29405b3683340bae1931fbf2024b2
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-14 17:14:35 +1000

    task 6 fixed logic

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,23 @@
  * puts2 - prints every other character of a string starting from 1st char, followed by new line
  * @str: pointer to a string
  *
- * Return: null
+ * Return: nothing
  */
 void puts2(char *str)
 {
     int counter;
-
+	int length;
+	
+	length = 0;
     counter = 0;
-    while(str[counter] != '\0')
+    while (str[counter] != '\0')
     {
-        _putchar(str[counter]);
-        counter++;
-        counter++;
+        counter += 1;
+	}
+	length = counter;
+	while (str[counter] != '\0' || str[counter] >= length)
+    {
+        counter += 2;
 	}
     _putchar('\n');
 }

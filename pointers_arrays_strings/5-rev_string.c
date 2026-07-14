9f6dc832a0b95f83d12d58295d750fb637ef4cf0
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-14 16:04:43 +1000

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to a char
+ *
+ * Return: null
+ */
+void rev_string(char *s)
+{
+    int counter;
+    
+
+	counter = 0;
+	while (s[counter] != '\0')
+    {
+		counter++;
+    }
+	while (counter >= 0)
+	{
+		s[counter] = s;
+		counter--;
+	}
+}

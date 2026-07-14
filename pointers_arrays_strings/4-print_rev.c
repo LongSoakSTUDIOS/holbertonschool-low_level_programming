312efed6d204e8b27a4f0ba5e9f643096b0f5448
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-14 15:52:55 +1000

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,13 @@ void print_rev(char *s)
 	int counter;
 
 	counter = 0;
-    while ( s[counter] != '\0')
+	while (s[counter] != '\0')
     {
-        counter++;
+		counter++;
     }
 	while (counter >= 0)
 	{
 		_putchar(s[counter]);
 		counter--;
 	}
-	_putchar('\n');
 }

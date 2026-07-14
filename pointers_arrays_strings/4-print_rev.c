7c5bf4c8bc71830e1b3c34a93b18adba6585d56d
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-14 15:51:03 +1000

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to a char
+ *
+ * Return: null
+ */
+void print_rev(char *s)
+{
+	int counter;
+
+	counter = 0;
+    while ( s[counter] != '\0')
+    {
+        counter++;
+    }
+	while (counter >= 0)
+	{
+		_putchar(s[counter]);
+		counter--;
+	}
+	_putchar('\n');
+}

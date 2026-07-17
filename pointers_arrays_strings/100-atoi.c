0bc4ec2f55fdb16a1ff620279d1110700c50d972
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-17 16:25:21 +1000

    task 10 update

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,23 +13,22 @@ int _atoi(char *s)
     counter = 0;
     res = 0;
 	sign = 1;
-	if (s[counter] == '-')
+	while(s[counter != '\0'])
 	{
-		sign = -1;
-		counter++;
+		if (s[counter] == '-')
+		{
+			sign = -1;
+			counter++;
+		}
+		if (s[counter] == '+')
+			counter++;
+		if (s[counter] >= '0' && s[counter] <= '9' )
+		{
+			res = res * 10 + (s[counter] - '0');
+			counter++;
+		}
+		if (s[counter] < '0' && s[counter] > '9' )
+			counter++;
 	}
-	if (s[counter] == '+')
-	{
-		counter++;
-	}
-    if (s[counter] >= '0' && s[counter] <= '9' )
-    {
-        res = res * 10 + (s[counter] - '0');
-        counter++;
-    }
-	if (s[counter] < '0' && s[counter] > '9' )
-	{
-		counter++;
-	}
-    return (res * sign);
+	return (res * sign);
 }

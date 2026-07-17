94e3b706657f0e04adb8d2d6d9dbf43a58195992
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-17 23:19:40 +1000

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * strncat - concatenates two strings
+ * @dest: pointer to string
+ * @src: pointer to string
+ * @int: amount of bytes avaliable
+ *
+ * Return: pointer to string
+ */
+char *strncat(char *dest, char *src, int n)
+{
+    int length1;
+    int length2;
+    int counter;
+
+    length1 = 0;
+    length2 = 0;
+    counter = 0;
+    while (dest[length1] != '\0')
+        length1++;
+    while (src[length2] != '\0')
+        length2++;
+    while(counter <= length2)
+    {
+        if (counter = n)
+            return (dest);
+        dest[length1 + counter] = src[counter];
+        counter++;
+    }
+    return (dest);
+}

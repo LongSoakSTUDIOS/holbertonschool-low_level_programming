1930f6ba5dacf0fa71ff5644f62d0e16af16e628
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-17 23:30:50 +1000

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+/**
+ * *_strncopy - concatenates two strings
+ * @dest: pointer to string
+ * @src: pointer to string
+ * @int: amount of bytes avaliable
+ *
+ * Return: pointer to string
+ */
+char *_strncopy(char *dest, char *src, int n)
+{
+    int counter;
+    int length;
+
+    counter = 0;
+    length = 0;
+    while (src[counter] != '\0')
+    {
+        counter++;
+    }
+    length = counter;
+    while (counter <= length)
+    {
+        if (counter > length && counter <= n)
+        { 
+            dest[counter] = '\0';
+            counter++;
+        }
+        dest[counter] = src[counter];
+        counter++;
+    }
+    return (dest);
+}

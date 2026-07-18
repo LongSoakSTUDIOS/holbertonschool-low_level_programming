b909c15e8103d46f9144c9da6a4940afc08ec8dc
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-18 20:34:46 +1000

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,22 @@
 char *cap_string(char *str)
 {
     int counter;
+    int toCap;
 
+    toCap = 0;
     counter = 0;
     while (counter != '\0')
     {
-        if (str[counter] >= 'a' && str[counter] <= 'z')
+        if (str[counter] >= 'a' && str[counter] <= 'z' && toCap == 0)
         {
             str[counter] = str[counter] - ('a' - 'A');
             counter++;
+            toCap = 1;
+        }
+        else if (str[counter] >= 'SP' && str[counter] <= '/')
+        {
+            counter++;
+            toCap = 0;
         }
         else
             counter++;

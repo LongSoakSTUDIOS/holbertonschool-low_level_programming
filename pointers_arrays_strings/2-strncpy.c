23570612b0a14ac729d789015ffbb011ff16fb80
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-18 12:04:08 +1000

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -18,6 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
         counter++;
     }
     length = counter;
+    counter = 0;
     while (counter <= length)
     {
         dest[counter] = src[counter];

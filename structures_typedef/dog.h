690853c525d2e822e543fe92f5c74d9c91a31b1a
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-22 20:39:12 +1000

    rename directory

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information for individual dogs
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: this struct contains 3 different data types in relation to dogs
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+

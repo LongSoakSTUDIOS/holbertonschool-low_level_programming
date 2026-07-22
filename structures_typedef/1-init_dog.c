e35ae3cc6d3af9d40dfde4783eb5bc5406767400
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-22 21:20:46 +1000

    updated null case to exit(139)

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * init_dog - initialize a variable of type struct dog
  *@d: struct
@@ -12,7 +13,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return (NULL);
+		exit(139);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

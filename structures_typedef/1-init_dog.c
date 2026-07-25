aab734cfee2a12f416124bfd630402f02b9e2cd7
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-25 16:10:23 +1000

    null case for name and owner

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,9 +13,13 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		exit(139);
+		return;
+	if (name == NULL)
+		return;
+	if (owner == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-}
+
 

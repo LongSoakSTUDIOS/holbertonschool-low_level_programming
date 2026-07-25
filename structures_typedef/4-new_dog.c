31d68cd3bf9a8f7f4699950ad0aa82990659cf3d
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-25 13:17:18 +1000

    freed and stored chars

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,14 +12,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog2;
+	char *dogName;
+	char *dogOwner;
+
+	dogName = name;
+	dogOwner = owner;
 
 	dog2 = malloc(sizeof(dog_t));
 	if (dog2 == NULL)
+	{
+		free(dog2);
 		return (NULL);
+	}
 
-	dog2->name = name;
+	dog2->name = dogName;
 	dog2->age = age;
-	dog2->owner = owner;
+	dog2->owner = dogOwner;
 
 	return (dog2);
 }

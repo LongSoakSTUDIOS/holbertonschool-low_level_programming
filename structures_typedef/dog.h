44c539e58ef02ea9ee302561e19bd46d8dad935c
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-25 15:56:04 +1000

    updating header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef DOG_H
+u#ifndef DOG_H
 #define DOG_H
 /**
  * struct dog - information for individual dogs
@@ -17,4 +17,5 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

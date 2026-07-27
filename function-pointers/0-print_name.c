6ddcfc0c3b3ab5312d9093db24a365375e51898c
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-27 15:34:40 +1000

    initial commit, task 0 and readme

---
+++ function-pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - prints a name
+* @name: pointer to char
+* @f: pointer to function
+*
+* Return: null
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);	
+}
+

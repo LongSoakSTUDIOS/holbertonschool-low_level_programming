54256c673f563dcefac7e6b39b855d3e547fcab1
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-27 16:04:41 +1000

    adding null cases

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,6 +14,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]) == 0)

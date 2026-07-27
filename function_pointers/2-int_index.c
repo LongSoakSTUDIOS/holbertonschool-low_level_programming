18247c03c700879f024d5ba9b9d98babfe2d75ca
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-27 16:02:16 +1000

    rename folder

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+/**
+* int_index - searches for an integer
+* @array: pointer to array
+* @size: size of array
+* @cmp: pointer to function to compare values
+*
+* Return: index of first 0 return from cmp, -1 on fail
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	i = 0;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]) == 0)
+		{
+			i++;
+		}
+		else
+			return (i);
+	}
+	return (-1);
+}
+

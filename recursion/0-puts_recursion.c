37f5ef1ca199a5f75257282fb0734b39704fc98e
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-31 21:01:18 +1000

    working task 0, betty complaint

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string via recursion
+ * @s: string to print
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		printf("\n");
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}

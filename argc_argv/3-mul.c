58387620f454a7af441519baa7ece73f9095476d
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-31 11:31:56 +1000

    moved up error case

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,14 +12,14 @@ int main(int argc, char **argv)
 	int num1;
 	int num2;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	sum = (num1 * num2);
 	if (argc < 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	sum = (num1 * num2);
 	printf("%d\n", sum);
 	return (0);
 }

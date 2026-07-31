7a5a8ac7236656235ec00e14fa0d716a962d0661
Nicky B <longsoakSTUDIOS@gmail.com>
2026-07-31 12:03:29 +1000

    task 4 working and betty compliant

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: count of args passed
+ * @argv: pointer to array of passed args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+	int nextNum;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	i = 1;
+	sum = 0;
+	while (i < argc)
+	{
+		nextNum = atoi(argv[i]);
+		if (nextNum == 0)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum = sum + nextNum;
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}

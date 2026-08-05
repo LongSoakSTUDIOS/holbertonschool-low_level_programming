7570bb6e720fefca16729be1564318b406eeb19a
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-05 10:01:31 +1000

    adding description to header

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,14 @@
 #ifndef LIST_H
 #define LIST_H
 
+/**
+ * struct lists_s - node holding a str, its length
+ * @str: poninter to a string
+ * @len: length of the string
+ * @next: next node
+ * 
+ * Description: a node that will hold a string and the strings length
+ */
 typedef struct list_s
 {
     char *str;

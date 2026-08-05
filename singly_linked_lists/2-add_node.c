a46be1663d7e8c437384aa025d5a857d931b70b6
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-05 12:49:15 +1000

    replacing strlen with a loop

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,7 +14,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	char *dup;
+	unsigned int i;
 
+	i = 0;
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
@@ -25,8 +27,13 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	while (dup[i] != '\0')
+	{
+		i++;
+	}
+	
 	new_node->str = dup;
-	new_node->len = strlen(dup);
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);

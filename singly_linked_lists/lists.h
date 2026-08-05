b179fd83bbc88513cd892bbb14a5ad838248128f
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-05 10:05:13 +1000

    betty compliant header

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,18 +2,18 @@
 #define LIST_H
 
 /**
- * struct lists_s - node holding a str, its length
+ * struct list_s - node holding a str, its length
  * @str: poninter to a string
  * @len: length of the string
  * @next: next node
- * 
+ *
  * Description: a node that will hold a string and the strings length
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);

552dc5eefdf1bdf289ffd33db4d68a6a93f96112
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-07 08:49:09 +1000

    betty complaint

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,32 +3,39 @@
 
 /**
  * is_prime_number - calculates if number is a prime number
- * @n: number to find sqroot of
- * 
+ * @n: number to find if prime
+ *
  * Return: 1 if prime number, 0 if not
  */
 int is_prime_number(int n)
 {
-    if (n < 2)
-    {
-        return (0);
-    }
-    return (check_div(n, 2));
-    
+	if (n < 2)
+	{
+		return (0);
+	}
+	return (check_div(n, 2));
+
 }
 
+/**
+ * check_div - calculates if number is a prime number
+ * @n: number to check prime of
+ * @div: current divisor
+ *
+ * Return: 1 if prime number, 0 if not
+ */
 int check_div(int n, int div)
 {
-    if (div * div > n)
-    {
-        return (1);
-    }
+	if (div * div > n)
+	{
+		return (1);
+	}
 
-    if (n % div == 0)
-    {
-        return (0);
-    }
-    return (check_div(n, div + 1));
-    
+	if (n % div == 0)
+	{
+		return (0);
+	}
+	return (check_div(n, div + 1));
+
 }
 

faa0b2de16b4735623c4a5540e55b1ccb22c0801
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-10 20:26:39 +1000

    updated to structure null list properly

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,10 +16,19 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *last_node;
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return NULL;
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
+		return (new_node);
 	}
 
 	last_node = *head;
@@ -28,9 +37,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		last_node = last_node->next;
 	}
 	last_node->next = new_node;
-
-	new_node->n = n;
-	new_node->next = NULL;
 	new_node->prev = last_node;
 
 	return (new_node);

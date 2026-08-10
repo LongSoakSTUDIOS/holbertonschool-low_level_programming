307f9cebb9dfab6f4b3b05da8d02aad5e82e2969
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-10 16:57:52 +1000

    task 1 working and betty complaint

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in the list
+ * @h: head of list
+ *
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *tmp = h;
+	int i;
+
+	i = 0;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}

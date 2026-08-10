a360de195ed18c3d34de62edefcd601e25e650f5
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-10 21:13:38 +1000

    task 5 working and betty complaint

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns nth node
+ * @head: head of list
+ * @index: index of node, from 0
+ *
+ * Return: address of new element, NULL on failure
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head->next != NULL)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+		head = head->next;
+		i++;
+	}
+
+	return (NULL);
+}

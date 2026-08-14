3792cc730919c95f0ce6e124e021625762e1f4ad
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-14 12:48:28 +1000

    initial commit + task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the hash table or NULL on failure
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i = 0;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		ht->array[i] = NULL;
+		i++;
+	}
+
+	ht->size = size;
+
+	return (ht);
+}

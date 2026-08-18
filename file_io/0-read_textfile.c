4a75d86ff55676fff4dd010fe37e59abe42e6572
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-18 17:47:45 +1000

    initial commit and task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints to stout
+ * @filename: name of file to read
+ * @letters: number of letters to read and print
+ * 
+ * Return - 0 on failure, 
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int fd;
+	ssize_t w, r;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	w = write(1, buf, r);
+	if (w == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+
+	return w;
+}

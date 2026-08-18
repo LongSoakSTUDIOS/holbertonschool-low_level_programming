f80c215dc6c9154ec2ffd81a3ddae7225fbe4741
Nicky B <longsoakSTUDIOS@gmail.com>
2026-08-18 20:01:03 +1000

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+/**
+ * create_file - reads a text file and prints to stout
+ * @filename: name of file to create
+ * @text_content: null terminated string to write
+ *
+ * Return: 1 on sucess, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	mode_t mode = S_IRUSR | S_IWUSR;
+	int fd, w;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
